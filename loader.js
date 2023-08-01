5fd84d0b47cd67ff53c4b742bb7a49b5e3db280a
naser.papi <uCV4@l5n6VD8>
2023-08-01 17:08:34 +0330

    runCommonTasks part 2

--- loader.js
+++ loader.js
@@ -1,11 +1,11 @@
 import addToCard from "./addToCard.js";
 import {runCommonTasks} from "./shared.js";
 const loader = ()=>{
-    const isAdded = sessionStorage.getItem("isMaltinaKeyAdded");
-    if (isAdded && isAdded==="YES"){
-        return;
-    }
-    sessionStorage.setItem("isMaltinaKeyAdded","YES");
+    // const isAdded = sessionStorage.getItem("isMaltinaKeyAdded");
+    // if (isAdded && isAdded==="YES"){
+    //     return;
+    // }
+    // sessionStorage.setItem("isMaltinaKeyAdded","YES");
     runCommonTasks();
     addToCard();
 }
