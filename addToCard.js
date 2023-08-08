8ccd665e3438e94ca0ff84c8f1f14dfde73e9bca
naser.papi <uCV4@l5n6VD8>
2023-08-08 11:56:24 +0330

    changed call api way part 2.

--- addToCard.js
+++ addToCard.js
@@ -31,7 +31,7 @@ window.calculateNewPrice =  async function (weight){
     }
     const resp = await fetch(constants.CALCULATE_COST_API,{
         method:"POST",
-        mode: "*cors", // no-cors, *cors, same-origin
+        mode: "cors", // no-cors, cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         headers: {
             "Content-Type": "application/json",
